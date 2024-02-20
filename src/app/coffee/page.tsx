2ec561822b77326b6adada415f211cc2ad98d569
Harshvardhan Makwana <harshvardhanmakwana786@gmail.com>
2024-02-20 15:57:06 +0530

    Navbar Added

--- src/app/coffee/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function CoffeePage(){
-    return(
-        <h1 className="text-3xl text-center mt-8">Coffee page</h1>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-2xl text-center">Harshvardhan</h1>
+    </main>
   );
 }
